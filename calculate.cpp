f5c2e9f66ebe0ea100054646e89fdcc26f983e68
Qingzhu-Xie <117921728+Qingzhu-Xie@users.noreply.github.com>
2022-11-28 22:14:19 +0800

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -1,42 +1,55 @@
-//calculate.cpp
-
 #include<iostream>
-#include<cmath>
 using namespace std;
 
 //transfer the original number to GPA
 double transfer(int n){
     if (n == 2)
-        return 1
-    if (n == 4)
-        return 1.3
-    if (n == 8)
-        return 1.7
-    if (n == 16)
-        return 2
-    if (n == 32)
-        return 2.3
-    if (n == 64)
-        return 2.7
-    if (n == 128)
-        return 3
-    if (n == 256)
-        return 3.3
-    if (n == 512)
-        return 3.7
-    if (n == 1024)
-        return 4
-    if (n == 2048)
-        return 4.3
+        return 1;
+    else if (n == 4)
+        return 1.3;
+    else if (n == 8)
+        return 1.7;
+    else if (n == 16)
+        return 2;
+    else if (n == 32)
+        return 2.3;
+    else if (n == 64)
+        return 2.7;
+    else if (n == 128)
+        return 3;
+    else if (n == 256)
+        return 3.3;
+    else if (n == 512)
+        return 3.7;
+    else if (n == 1024)
+        return 4;
+    else if (n == 2048)
+        return 4.3;
+    else 
+        return 0;
 }
 
 //calculate the score of current game table
 double calculation(const vector<vector<int>>& v, const int size){
     double score = 0;
+    int cntava = 0;
+    int intpart = 0;
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             score += transfer(v[i][j]);
         }
     }
-    return score;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if (v[i][j] != 0)
+            cntava += 1;
+        }
+    }
+    if (cntava == 0)
+        return 0;
+    else{
+        score = score / cntava;
+        intpart = (score + 0.005) / 0.01;
+        score = intpart * 0.01 ;
+        return score;}
 }
