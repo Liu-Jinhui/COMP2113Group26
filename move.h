5eb86ec6166b754f75974264d384654d76e33455
Qingzhu-Xie <117921728+Qingzhu-Xie@users.noreply.github.com>
2022-11-29 22:52:52 +0800

    Update move.h

--- move.h
+++ move.h
@@ -3,9 +3,9 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-void up();
-void down();
-void left();
-void right();
+void up(vector<vector<int>>& v, const int size);
+void down(vector<vector<int>>& v, const int size);
+void left(vector<vector<int>>& v, const int size);
+void right(vector<vector<int>>& v, const int size);
 
 #endif
