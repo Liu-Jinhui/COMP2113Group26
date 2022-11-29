396084c9e319d8d44005a36cd8e7f169820a11cc
Qingzhu-Xie <117921728+Qingzhu-Xie@users.noreply.github.com>
2022-11-29 22:46:52 +0800

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -5,18 +5,186 @@
 #include "move.h"
 using namespace std;
 
-void up(){
-    return ;
+void up(vector<vector<int>>& v, const int size){
+    vector<int> current_col, new_col;
+    int col_size;
+    for(int j = 0; j < size; j++){
+        while (!current_col.empty()){
+            current_col.pop_back();
+        }
+        while (!new_col.empty()){
+            new_col.pop_back();
+        }
+        for(int i = 0; i < size; i++){
+            if(v[i][j] != 0){
+                current_col.push_back(v[i][j]);
+            }
+        }
+        if(current_col.size() > 1){
+            col_size = current_col.size();
+            int k = 0;
+            while(k < col_size){
+                if(k < col_size - 1 && current_col[k] == current_col[k+1]){
+                    new_col.push_back(current_col[k]*2);
+                    k += 2;
+                }
+                else{
+                    new_col.push_back(current_col[k]);
+                    k++;
+                }
+            }
+        }
+        else if(current_col.size() == 1){
+            new_col.push_back(current_col.front());
+            current_col.erase(current_col.begin());
+        }
+        col_size = new_col.size();
+        for(int i = 0; i < size; i++){
+            if(i < col_size){
+                v[i][j] = new_col.front();
+                new_col.erase(new_col.begin());
+            }
+            else {
+                v[i][j] = 0;
+            }
+        }
+    }
 }
 
-void down(){
-    return ;
+void down(vector<vector<int>>& v, const int size){
+    vector<int> current_col, new_col;
+    int col_size;
+    for(int j = 0; j < size; j++){
+        while (!current_col.empty()){
+            current_col.pop_back();
+        }
+        while (!new_col.empty()){
+            new_col.pop_back();
+        }
+        for (int i = size - 1; i >= 0; i--){
+            if(v[i][j] != 0){
+                current_col.push_back(v[i][j]);
+            }
+        }
+        int k = 0;
+        if(current_col.size() > 1){
+            col_size = current_col.size();
+            int k = 0;
+            while(k < col_size){
+                if(k < col_size - 1 && current_col[k] == current_col[k+1]){
+                    new_col.push_back(current_col[k]*2);
+                    k += 2;
+                }
+                else{
+                    new_col.push_back(current_col[k]);
+                    k++;
+                }
+            }
+        }
+        else if(current_col.size() == 1){
+            new_col.push_back(current_col.front());
+            current_col.erase(current_col.begin());
+        }
+        for(int i = size - 1; i >=0; i--){
+            if(!new_col.empty()){
+                v[i][j] = new_col.front();
+                new_col.erase(new_col.begin());
+            }
+            else{
+                v[i][j] = 0;
+            }
+        }
+    }
 }
 
-void left(){
-    return ;
+void left(vector<vector<int>>& v, const int size){
+    vector<int> current_row, new_row;
+    int row_size;
+    for(int i = 0; i < size; i++){
+        while (!current_row.empty()){
+            current_row.pop_back();
+        }
+        while (!new_row.empty()){
+            new_row.pop_back();
+        }
+        for (int j = 0; j < size; j++){
+            if(v[i][j] != 0){
+                current_row.push_back(v[i][j]);
+            }
+        }
+        int k = 0;
+        if(current_row.size() > 1){
+            row_size = current_row.size();
+            int k = 0;
+            while(k < row_size){
+                if(k < row_size - 1 && current_row[k] == current_row[k+1]){
+                    new_row.push_back(current_row[k]*2);
+                    k += 2;
+                }
+                else{
+                    new_row.push_back(current_row[k]);
+                    k++;
+                }
+            }
+        }
+        else if(current_row.size() == 1){
+            new_row.push_back(current_row.front());
+            current_row.erase(current_row.begin());
+        }
+        for(int j = 0; j < size; j++){
+            if(!new_row.empty()){
+                v[i][j] = new_row.front();
+                new_row.erase(new_row.begin());
+            }
+            else{
+                v[i][j] = 0;
+            }
+        }
+    }
 }
 
-void right(){
-    return ;
+void right(vector<vector<int>>& v, const int size){
+    vector<int> current_row, new_row;
+    int row_size;
+    for(int i = 0; i < size; i++){
+        while (!current_row.empty()){
+            current_row.pop_back();
+        }
+        while (!new_row.empty()){
+            new_row.pop_back();
+        }
+        for (int j = size - 1; j >= 0; j--){
+            if(v[i][j] != 0){
+                current_row.push_back(v[i][j]);
+            }
+        }
+        int k = 0;
+        if(current_row.size() > 1){
+            row_size = current_row.size();
+            int k = 0;
+            while(k < row_size){
+                if(k < row_size - 1 && current_row[k] == current_row[k+1]){
+                    new_row.push_back(current_row[k]*2);
+                    k += 2;
+                }
+                else{
+                    new_row.push_back(current_row[k]);
+                    k++;
+                }
+            }
+        }
+        else if(current_row.size() == 1){
+            new_row.push_back(current_row.front());
+            current_row.erase(current_row.begin());
+        }
+        for(int j = size - 1; j >= 0; j--){
+            if(!new_row.empty()){
+                v[i][j] = new_row.front();
+                new_row.erase(new_row.begin());
+            }
+            else{
+                v[i][j] = 0;
+            }
+        }
+    }
 }
