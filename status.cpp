b88c90d6f32c00fb725a6cc413e36241fee2c8cf
Jinhui Liu <96280063+Liu-Jinhui@users.noreply.github.com>
2022-11-30 21:45:52 +0800

    Update status.cpp

--- status.cpp
+++ status.cpp
@@ -5,8 +5,8 @@
 #include "status.h"
 using namespace std;
 
-bool check_alive(const vector<vector<int>>&v, const int size){
-    bool flag = false;
+int check_alive(const vector<vector<int>>&v, const int size){
+    int flag = 0;
     int cnt = 0;
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
@@ -14,17 +14,17 @@ bool check_alive(const vector<vector<int>>&v, const int size){
         }
     }
     if(cnt < size * size){
-        flag = true;
+        flag = 1;
     }
     else{
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size - 1; j++){
-                if(v[i][j] && v[i][j] == v[i][j+1]) flag = true;
+                if(v[i][j] && v[i][j] == v[i][j+1]) flag = 34;
             }
         }
         for(int j = 0; j < size; j++){
             for(int i = 0; i < size - 1; i++){
-                if(v[i][j] && v[i][j] == v[i + 1][j]) flag = true;
+                if(v[i][j] && v[i][j] == v[i + 1][j]) flag = 12;
             }
         }
     }
